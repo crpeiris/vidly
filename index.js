8bb1c6b3df2e4c4e8b1adee576b9a411e57453c7
Ranil Peiris <cranil@sjp.ac.lk>
2023-10-23 12:35:20 +0200

    V3 Debugger added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Build a web server on port 3000
+const morgan =require('morgan');
+const debug =require('debug');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
@@ -6,7 +8,12 @@ app.use(express.json());
 const genres = require('./routes/genres');
 const port=3000;
 
+console.log(app.get('tiny'));
 
+if (app.get('env')==='development'){
+    app.use(morgan('dev'));
+    debug ('morgan enabled...');
+}
 app.use('/api/genres',genres);
 
 //Listen to port
